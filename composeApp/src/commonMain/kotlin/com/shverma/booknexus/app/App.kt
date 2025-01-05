397a0d981bb2757f2ae3e0cd809a919ddd6d4e56
sh7verma <shubham.verma740@gmail.com>
2025-01-05 19:49:56 +0530

    Navigation

--- composeApp/src/commonMain/kotlin/com/shverma/booknexus/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/shverma/booknexus/app/App.kt
@@ -1,12 +1,24 @@
 package com.shverma.booknexus.app
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.shverma.booknexus.book.presentation.book_list.BookListScreenRoot
 import com.shverma.booknexus.book.presentation.book_list.BookListViewModel
+import kotlinx.serialization.Serializable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -14,12 +26,48 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 @Preview
 fun App() {
-    BookListScreenRoot(
-        viewModel = koinViewModel<BookListViewModel>(),
-        onBookClick = { book ->
+    MaterialTheme {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = Route.BookGraph
+        ) {
+            navigation<Route.BookGraph>(
+                startDestination = Route.BookList
+            ) {
+                composable<Route.BookList> {
+                    BookListScreenRoot(
+                        viewModel = koinViewModel<BookListViewModel>(),
+                        onBookClick = { book ->
+                            navController.navigate(
+                                Route.BookDetail(book.id)
+                            )
+                        }
+                    )
+                }
+                composable<Route.BookDetail> { entry ->
+                    val arg = entry.toRoute<Route.BookDetail>()
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text(text = "Book Details screen! The ID is ${arg.id}")
+                    }
+                }
+            }
         }
-    )
+    }
 }
+
+sealed interface Route {
+
+    @Serializable
+    data object BookGraph : Route
+
+    @Serializable
+    data object BookList : Route
+
+    @Serializable
+    data class BookDetail(val id: String) : Route
+}
+
 
 @Composable
 private inline fun <reified T : ViewModel> NavBackStackEntry.sharedKoinViewModel(
